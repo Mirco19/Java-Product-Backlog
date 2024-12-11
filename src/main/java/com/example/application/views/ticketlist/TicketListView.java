30a8d1ed78b79173a9aaaad93601f7a0f218e5cb
start.vaadin.com <no-reply@vaadin.com>
2024-12-11 20:11:09 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/ticketlist/TicketListView.java
@@ -0,0 +1,33 @@
+package com.example.application.views.ticketlist;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+@PageTitle("TicketList")
+@Route("")
+@Menu(order = 0, icon = LineAwesomeIconUrl.PENCIL_RULER_SOLID)
+public class TicketListView extends Composite<VerticalLayout> {
+
+    public TicketListView() {
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        VerticalLayout layoutColumn3 = new VerticalLayout();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        layoutColumn2.setWidth("100%");
+        layoutColumn2.getStyle().set("flex-grow", "1");
+        layoutColumn3.getStyle().set("flex-grow", "1");
+        getContent().add(layoutRow);
+        layoutRow.add(layoutColumn2);
+        layoutRow.add(layoutColumn3);
+    }
+}
